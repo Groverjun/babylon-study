18901dc7e5f5d1ccee6fb45376fe56f65ddad203
LAPTOP-G3218HKM\longruan <lijunjie1@longruan.com>
2026-03-11 14:04:11 +0800

    add

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/babylon-study/",
   plugins: [
     vue(),
     // vueDevTools(),
